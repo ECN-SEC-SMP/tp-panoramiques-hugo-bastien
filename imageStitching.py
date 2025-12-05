ccd4768655b7f8abfd317d9bb25d4afd831c2214
BastienFelix <bastien.felix44@gmail.com>
2025-12-05 12:11:51 +0100

    Image stitching

--- imageStitching.py
+++ imageStitching.py
@@ -10,13 +10,16 @@ Q_KEY = 113
 
 def parse_command_line_arguments():# Parse command line arguments
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-k", "--kp", default="GFTT", help="key point (or corner) detector: GFTT ORB SIFT ")
-    parser.add_argument("-n", "--nbKp", default=1000, type=int, help="Number of key point required (if configurable) ")
+    parser.add_argument("-k", "--kp", default="SIFT", help="key point (or corner) detector: GFTT ORB SIFT BRIEF")
+    parser.add_argument("-n", "--nbKp", default=100, type=int, help="Number of key point required (if configurable) ")
+    parser.add_argument("-e", "--extractor", default=None, help="descriptor extractor (if None, uses detector's descriptor)")
     parser.add_argument("-d", "--descriptor", default=True, type=bool, help="compute descriptor associated with detector (if available)")
-    parser.add_argument("-m", "--matching", default="NORM_L1", help="Brute Force norm: NORM_L1, NORM_L2, NORM_HAMMING, NORM_HAMMING2")
+    parser.add_argument("-m", "--matching", default="NORM_HAMMING2", help="Brute Force norm: NORM_L1, NORM_L2, NORM_HAMMING, NORM_HAMMING2")
     parser.add_argument("-i1", "--image1", default="IMG_1_reduced.jpg", help="path to image1")
     parser.add_argument("-i2", "--image2", default="IMG_2_reduced.jpg", help="path to image2")
     parser.add_argument("-a", "--alpha", default=10, type=float, help="Multiplicative coefficient for distance threshold (alpha * distMin)")
+    parser.add_argument("-min", "--minMatch", default=10, type=int, help="Min match count for homography")
+
     return parser
 
 def test_load_image(img):
@@ -54,15 +57,20 @@ def feature_detector(type, gray, nb):
                 kp = orb.detect(gray, None)
             case "SIFT":
                 sift = cv.SIFT_create(nb)
-                kp=sift.detect(gray, None)
+                kp = sift.detect(gray, None)
+            case "BRIEF":
+                star = cv.xfeatures2d.StarDetector_create()
+                kp = star.detect(gray, None)
+                if len(kp) > nb:
+                    kp = sorted(kp, key=lambda x: -x.response)[:nb]
     else:
         kp =  None
     return kp
 
-def feature_extractor(type, img, kp):
+def feature_extractor(detector_type, img, kp, extractor_type=None):
     desc = None
     if img is not None and kp is not None:
-        match type:
+        match detector_type:
             case "GFTT":
                 detector = cv.ORB_create()
                 kp, desc = detector.compute(img, kp)
@@ -72,6 +80,9 @@ def feature_extractor(type, img, kp):
             case "SIFT":
                 detector = cv.SIFT_create()
                 kp, desc = detector.compute(img, kp)
+            case "BRIEF":
+                brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
+                kp, desc = brief.compute(img, kp)
     return kp, desc
 
 def feature_matching(matching, desc1, desc2, alpha):
@@ -99,8 +110,70 @@ def feature_matching(matching, desc1, desc2, alpha):
     
     return good
 
-def main():
+def homography(min_match, good, kp1, kp2, img1, img2):
+    if len(good) > min_match:
+        matches_flat = [m[0] for m in good]
+        
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in matches_flat]).reshape(-1,1,2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches_flat]).reshape(-1,1,2)
+        H, mask = cv.findHomography(dst_pts, src_pts, cv.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+        h, w = img1.shape[:2]
+        pts = np.float32([[0,0], [0,h-1], [w-1,h-1], [w-1,0]]).reshape(-1,1,2)
+        dst = cv.perspectiveTransform(pts, H)
+        img2_with_box = cv.polylines(img2.copy(), [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+        
+        draw_params = dict(matchColor = (0,255,0), 
+                       singlePointColor = None,
+                       matchesMask = matchesMask, 
+                       flags = 2)
+        
+        img3 = cv.drawMatches(img1, kp1, img2_with_box, kp2, matches_flat, None, **draw_params)
+        display_image(img3, "Homography Matches")
+        return H
+    else:
+        print("Not enough matches are found - {}/{}".format(len(good), min_match))
+        return None
+    
+def finalImageStitching(img1, img2, H):
+    if H is None:
+        print("Pas de matrice d'homographie")
+        return None
+    
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
 
+    pts1 = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
+    pts2 = np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)
+    
+    pts2_transformed = cv.perspectiveTransform(pts2, H)
+    
+    all_pts = np.concatenate((pts1, pts2_transformed), axis=0)
+    [x_min, y_min] = np.int32(all_pts.min(axis=0).ravel() - 0.5)
+    [x_max, y_max] = np.int32(all_pts.max(axis=0).ravel() + 0.5)
+    
+    translation = [-x_min, -y_min]
+    H_translation = np.array([[1, 0, translation[0]], 
+                               [0, 1, translation[1]], 
+                               [0, 0, 1]], dtype=np.float64)
+    
+    canvas_width = x_max - x_min
+    canvas_height = y_max - y_min
+    
+    output_img = cv.warpPerspective(img2, H_translation.dot(H), 
+                                     (canvas_width, canvas_height))
+    
+    y1, y2 = translation[1], translation[1] + h1
+    x1, x2 = translation[0], translation[0] + w1
+    
+    mask = output_img[y1:y2, x1:x2] != 0
+    output_img[y1:y2, x1:x2][~mask] = img1[~mask]
+    
+    display_image(output_img, 'Stitched Result')
+    return output_img
+
+
+def main():
     parser = parse_command_line_arguments()
     args = vars(parser.parse_args())
 
@@ -127,17 +200,14 @@ def main():
     display_image(img_kp1, "Image 1 "+args["kp"])
     if img2 is not None : display_image(img_kp2, "Image 2 "+args["kp"])
 
-    kp1, desc1 = feature_extractor(args["kp"], gray1, kp1)
-    if img2 is not None: kp2, desc2 = feature_extractor(args["kp"], gray2, kp2)
+    kp1, desc1 = feature_extractor(args["kp"], gray1, kp1, args["extractor"])
+    if img2 is not None: kp2, desc2 = feature_extractor(args["kp"], gray2, kp2, args["extractor"])
 
     good = feature_matching(args["matching"], desc1, desc2, args["alpha"])
     display_matching(img1, kp1, img2, kp2, good)
 
-    # code to complete (using functions):
-    # - to extract feature and compute descriptor with ORB and SIFT 
-    # - to calculate brute force matching between descriptor using different norms
-    # - to calculate and apply homography 
-    # - to stich and display resulting image
+    H = homography(args["minMatch"], good, kp1, kp2, gray1, gray2)
+    finalImageStitching(img1, img2, H)
 
     # waiting for user action
     key = 0
